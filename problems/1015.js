0fe5933ab83609c1bafb436d998ec531cd96552a
Fernando Leonid <fernandoleonid@gmail.com>
2024-06-05 16:43:38 -0300

    1015 delte space

--- problems/1015.js
+++ problems/1015.js
@@ -2,8 +2,10 @@ export function problem (lines){
     const [x1, y1] = lines[0].split(' ').map(Number)
     const [x2, y2] = lines[1].split(' ').map(Number)
 
+    console.log (x2 -  x1, y2 - y1 )
+
     const distancia = Math.sqrt(Math.pow(x2 -  x1, 2) + Math.pow(y2 - y1, 2))
 
     console.log (distancia.toFixed(4))
-    
+
 }
