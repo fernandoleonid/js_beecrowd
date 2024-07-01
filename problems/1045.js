d33642cf4e4dcdd85582a34f4d7d37d5cf72bc37
Fernando Leonid <fernandoleonid@gmail.com>
2024-07-01 09:36:42 -0300

    1045

---
+++ problems/1045.js
@@ -0,0 +1,38 @@
+export function problem (lines){
+    let [a, b, c] = lines[0].split(' ').map(Number)
+    
+    const ordenarLados = (lados) => lados.sort((x, y) => y - x)
+
+    const verificarTriangulo = ([a, b, c]) => a < b + c
+
+    function classicarTringualo ([a, b, c]) {
+        const mensagem = []
+        if (a*a == b*b + c*c ){
+            mensagem.push('TRIANGULO RETANGULO')
+        }
+        if (a*a > b*b + c*c ){
+            mensagem.push('TRIANGULO OBTUSANGULO')
+        }
+        if (a*a < b*b + c*c ){
+            mensagem.push('TRIANGULO ACUTANGULO')
+        }
+        if (a == b && b == c) {
+            mensagem.push('TRIANGULO EQUILATERO')
+        }else if(a == b || a == c || b == c){
+            mensagem.push('TRIANGULO ISOSCELES')
+        }
+        return mensagem.join('\n')
+    }
+
+    function principal(a, b, c){
+        const ladosOrdenados = ordenarLados([a, b, c])
+        if (verificarTriangulo(ladosOrdenados)){
+            const classificacao = classicarTringualo(ladosOrdenados)
+            console.log(classificacao)
+        }else{
+            console.log ('NAO FORMA TRIANGULO')
+        }
+    }
+
+    principal(a, b, c)
+}
