c47a2029a3d64355af645738f1b8580945de23bb
Fernando Leonid <fernandoleonid@gmail.com>
2024-07-09 21:49:49 -0300

    1047 e 1048

---
+++ problems/1047.js
@@ -0,0 +1,19 @@
+export function problem (lines){
+    const [horaInicial, minutoInicial, horaFinal, minutoFinal] = lines[0].split(' ').map(Number)
+
+    const inicio = horaInicial * 60 + minutoInicial
+    const fim = horaFinal * 60 + minutoFinal
+
+    let duracaoTotalMinutos
+
+    if (fim > inicio){
+        duracaoTotalMinutos = fim - inicio
+    }else{
+        duracaoTotalMinutos = (24 * 60 - inicio) + fim
+    }
+
+    const duracaoHoras = Math.floor(duracaoTotalMinutos / 60)
+    const duracaoMinutos = duracaoTotalMinutos % 60
+
+    console.log (`O JOGO DUROU ${duracaoHoras} HORA(S) E ${duracaoMinutos} MINUTO(S)`)
+} 
---
+++ problems/1048.js
@@ -0,0 +1,29 @@
+export function problem(lines){
+    
+    function calcularReajuste(salario){
+        const tabela = [
+            {limite: 400, percentual: 15},
+            {limite: 800, percentual: 12},
+            {limite: 1200, percentual: 10},
+            {limite: 2000, percentual: 7},
+            {limite: Infinity, percentual: 4}
+        ]
+
+        const faixaSalarial = tabela.find( faixa => salario <= faixa.limite)
+        const reajuste = salario * faixaSalarial.percentual / 100
+        const novoSalario = salario + reajuste
+
+        return {
+            salario: novoSalario.toFixed(2),
+            reajuste: reajuste.toFixed(2),
+            percentual: faixaSalarial.percentual
+        }
+    }
+
+    const salario = Number(lines[0])
+    const resultado = calcularReajuste(salario)
+
+    console.log (`Novo salario: ${resultado.salario}`)
+    console.log (`Reajuste ganho: ${resultado.reajuste}`)
+    console.log (`Em percentual: ${resultado.percentual} %`)
+}   
