301569752d4d3dbd2f86ba6df3d59359e098915a
Fernando Leonid <fernandoleonid@gmail.com>
2024-08-25 08:56:15 -0300

    bee 1050

---
+++ problems/1050.js
@@ -0,0 +1,19 @@
+export function problem (lines) {
+
+    const ddd = lines[0]
+    const dddCidades = {
+        "61": "Brasilia",
+        "71": "Salvador",
+        "11": "Sao Paulo",
+        "21": "Rio de Janeiro",
+        "32": "Juiz de Fora",
+        "19": "Campinas",
+        "27": "Vitoria",
+        "31": "Belo Horizonte"
+    }
+
+    const cidade = dddCidades[ddd] || "DDD nao cadastrado"
+
+    console.log (cidade)
+
+}
