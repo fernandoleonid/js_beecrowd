606451335b7353efca194cebef20a8661a94dbaa
professor Fernando <fernandoleonid@gmail.com>
2024-10-05 12:13:16 -0300

    1066

---
+++ problems/1066.js
@@ -0,0 +1,17 @@
+export function problem (lines){
+    const numeros = lines.slice(0,-1).map(Number)
+
+    const pares = numeros.filter(numero => numero % 2 == 0).length
+
+    const impares = numeros.filter(numero => numero % 2 !==0).length
+
+    const positivos = numeros.filter(numero => numero > 0).length
+
+    const negativos = numeros.filter(numero => numero < 0).length
+
+    console.log (`${pares} valor(es) par(es)`)
+    console.log (`${impares} valor(es) impar(es)`)
+    console.log (`${positivos} valor(es) positivo(s)`)
+    console.log (`${negativos} valor(es) negativo(s)`)
+
+}
