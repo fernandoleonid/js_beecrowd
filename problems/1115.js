8714c1286651be0276318d3326a15d8c14461fcf
professor Fernando <fernandoleonid@gmail.com>
2025-01-30 21:40:14 -0300

    1115

---
+++ problems/1115.js
@@ -0,0 +1,16 @@
+export function problem (lines) {
+    lines.forEach ( line => {
+        const [x, y] = line.split(' ').map(Number)
+        if (x === 0 || y === 0) {
+            return
+        }else if (x > 0 && y > 0) {
+            console.log ('primeiro')
+        }else if (x < 0 && y > 0) {
+            console.log ('segundo')
+        }else if (x < 0 && y < 0) {
+            console.log ('terceiro')
+        }else if (x > 0 && y < 0) {
+            console.log ('quarto')
+        }
+    })
+}
