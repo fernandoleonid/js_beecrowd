3253e559c551e8db5f7f30b0add14598a18d68f7
professor Fernando <fernandoleonid@gmail.com>
2025-02-02 08:08:49 -0300

    1117

---
+++ problems/1117.js
@@ -0,0 +1,18 @@
+export function problem(lines){
+    let notasValidas = 0
+    let soma = 0
+    lines.forEach( (line ) => {
+        const nota = Number(line)
+
+        if (nota < 0 || nota > 10) {
+            console.log ('nota invalida')
+        } else{
+            notasValidas++
+            soma += nota
+            if (notasValidas === 2) {
+                const media = (soma / 2).toFixed(2)
+                console.log (`media = ${media}`)
+            }
+        }
+    })
+}
